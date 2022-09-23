c6a6af2022340a6fdcd9be72ed7df63916c2d82b
julian ST <julian.salas.t7@gmail.com>
2022-09-23 17:32:00 -0500

    Se realizo la vista para citas

--- AppViajes/AppViajes/vista/Itinerario.xaml.cs
+++ AppViajes/AppViajes/vista/Itinerario.xaml.cs
@@ -15,16 +15,7 @@ namespace AppViajes.vista
         
         public Itinerario()
         {
-            InitializeComponent();
-            lista01.Items.Add("Mala");
-            lista01.Items.Add("Cañete");
-            lista01.Items.Add("Chincha");
-            lista01.Items.Add("Ica");
-
-            lista02.Items.Add("Mala");
-            lista02.Items.Add("Cañete");
-            lista02.Items.Add("Chincha");
-            lista02.Items.Add("Ica");
+            InitializeComponent();            
 
         }
 
