66fceb486ef6c31f46add430172114a107d84aaf
nezash <josemanuelosores@yahoo.com>
2022-09-23 18:53:25 -0500

    Avance

--- AppViajes/AppViajes/App.xaml.cs
+++ AppViajes/AppViajes/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using AppViajes.data;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,11 +12,20 @@ namespace AppViajes
         public App()
         {
             InitializeComponent();
+            crearBD();
             //Navigationpage es para navegar entre páginas de contenido
             MainPage = new NavigationPage(new MainPage());
         }
 
+        public static DbContexto contexto { get; set; }
 
+        private void crearBD()
+        {
+            //la BD se crea en el proyecto
+            var carpeta = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var data = System.IO.Path.Combine(carpeta, "Viajes.db3");
+            contexto = new DbContexto(data);
+        }
         //-------------------------------
         //        M É T O D O S
         //-------------------------------
---
+++ AppViajes/AppViajes/data/DbContexto.cs
@@ -0,0 +1,40 @@
+﻿using AppViajes.modelo;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppViajes.data
+{
+    public class DbContexto
+    {
+        private readonly SQLiteAsyncConnection cnx;
+        //Contructor
+        public DbContexto(String data)
+        {
+            cnx = new SQLiteAsyncConnection(data);
+            //Crear una tabla
+            cnx.CreateTableAsync<ClsCounters>().Wait();
+        }
+
+        // Métodos de mantenimientos
+        public async Task<int> ingresar(ClsCounters registro)
+        {
+            return await cnx.InsertAsync(registro);
+        }
+        public async Task<int> eliminar(int id)
+        {
+            return await cnx.DeleteAsync(id);
+        }
+        public async Task<List<ClsCounters>> GetCounters()
+        {
+            return await cnx.Table<ClsCounters>().ToListAsync();
+        }
+        public async Task<int> modificar(ClsCounters registro)
+        {
+            return await cnx.UpdateAsync(registro);
+        }
+
+    }
+}
