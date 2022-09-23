44496a1c897c8f53daffa111e129d6d05548e73e
nezash <josemanuelosores@yahoo.com>
2022-09-23 03:33:56 -0500

    Carrucel avance

---
+++ AppViajes/AppViajes/modelo/ClsImagenes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppViajes.modelo
+{
+    public class ClsImagenes
+    {
+        public string imagen{ get; set; }
+    }
+}
