d35504c80c705bf89d3aa06193cc28a1bd8a10af
nezash <josemanuelosores@yahoo.com>
2022-09-23 19:39:54 -0500

    Arreglo de conflictos

--- AppViajes/AppViajes/vista/Itinerario.xaml.cs
+++ AppViajes/AppViajes/vista/Itinerario.xaml.cs
@@ -12,19 +12,10 @@ namespace AppViajes.vista
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Itinerario : ContentPage
     {
-        
+
         public Itinerario()
         {
             InitializeComponent();
-            lista01.Items.Add("Mala");
-            lista01.Items.Add("Cañete");
-            lista01.Items.Add("Chincha");
-            lista01.Items.Add("Ica");
-
-            lista02.Items.Add("Mala");
-            lista02.Items.Add("Cañete");
-            lista02.Items.Add("Chincha");
-            lista02.Items.Add("Ica");
 
         }
 
